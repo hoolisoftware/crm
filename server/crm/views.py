65fa23a1f500c4099379479697e28a6753a6e383
lamas1901 <ilhanchanel20002@gmail.com>
2023-02-11 02:04:40 +0300

    Initial Commit

---
+++ server/crm/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views import generic
+
+class RegisterView:
+    pass
+
+class LoginView:
+    pass
+
+class UserProjectListView:
+    pass
+
+class CreateProjectView:
+    pass
+
+class JoinProjectView:
+    pass
