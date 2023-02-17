6c36c3f7f7a442bebfdc752d384161619ea0b535
Ilkhan <ilhanchanel20002@gmail.com>
2023-02-17 19:08:08 +0300

    Fixed theme folder

--- server/core/settings/common.py
+++ server/core/settings/common.py
@@ -4,7 +4,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
-CSRF_TRUSTED_ORIGINS = ['http://0.0.0.0', 'http://127.0.0.1', 'http://localhost']
+CSRF_TRUSTED_ORIGINS = [
+    'http://0.0.0.0',
+    'http://127.0.0.1',
+    'http://localhost'
+]
 
 if (HOST := environ.get('HOST')):
     ALLOWED_HOSTS.append(HOST)
